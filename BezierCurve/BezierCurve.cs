eed3a949e5bfb4e1edd40b825f65a7ca7da76da1
yaggod <yaggod@yandex.ru>
2021-09-09 15:28:41 +0300

    Добавьте файлы проекта.

---
+++ BezierCurve/BezierCurve.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BezierCurve
+{
+    class BezierCurve
+    {
+        private readonly Point[] points;
+        private int dots;
+        public int PtsCount { get => dots; }
+        public Point[] Result { get; }
+        public BezierCurve(IEnumerable<Point> points, int resultPointsCount)
+        {
+            this.points = points.ToArray();
+            dots = resultPointsCount;
+            Result = new Point[dots];
+            CalculatePath();
+
+        }
+
+
+        private static Point CalculateTwoPoints(Point p0, Point p1, float t)
+        {
+            return (1 - t) * p0 + t * p1;
+        }
+        private Point CalculatePoint(float t)
+        {
+            
+            int counter = 1;
+            Point[] temp = points;
+            while (counter < points.Length) {
+                for (int i = 0; i < temp.Length - counter; i++)
+                {
+                    temp[i] = CalculateTwoPoints(temp[i], temp[i + 1], t);
+                }
+                counter++;
+            }
+            return temp[0];
+        }
+        private void CalculatePath()
+        {
+            for (int t = 0; t < dots; t++)
+            {
+                Result[t] = CalculatePoint((float)t / (dots - 1));
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(Point p in Result)
+            {
+                sb.Append(p + " ");
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ BezierCurve/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BezierCurve
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickExit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Char.IsDigit(e.Text,0);   
+        }
+    }
+    
+    
+}
